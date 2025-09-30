453f16d83d314a66f8179fd1494f10583980a499
Divyanshi-20 <divyanshig087@gmail.com>
2025-10-01 00:34:02 +0530

    app.py

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ def create_app():
 
     return app
 
-
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     app.run(
         debug=True,
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
     )
+
